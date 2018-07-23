3590ede76c6d3ce31b4eb3cf6b5dcfba7df71024
kdandapani <37008571+kdandapani@users.noreply.github.com>
2018-07-24 00:27:45 +0530

    Update CukesTest.java

--- src/test/java/servlet/CukesTest.java
+++ src/test/java/servlet/CukesTest.java
@@ -7,7 +7,5 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({login_junit.class,createpage_junit.class})
 public class CukesTest {
-	 public static void main(String[] args){
-		 System.out.println("Processing CukesTest.java");
-	 }
+
 }
