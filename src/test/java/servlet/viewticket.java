091ac7fe586484fafa50a1719bc3e88f7d2f687e
sonali-dg2018 <41725145+sonali-dg2018@users.noreply.github.com>
2018-07-26 15:13:53 +0530

    Update viewticket.java
    Updated IP address in line 20

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://18.222.127.227:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://18.218.241.45:5432/postgres";
 			String username="postgres";
 			String password="";
 						
