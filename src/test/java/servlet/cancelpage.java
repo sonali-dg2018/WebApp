548426a25ede1ce9ae5856bcd606cd7b669ea225
sonali-dg2018 <41725145+sonali-dg2018@users.noreply.github.com>
2018-07-27 09:49:42 +0530

    Update cancelpage.java
    Updated IP address in line 20.

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://18.218.241.45:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://18.222.209.14:5432/postgres";
 					String username="postgres";
 					String password="";
 					
